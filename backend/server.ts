557e3038315f5cb106fb9cb3e54dd855d5903bd4
Chewsday <z647623@gmail.com>
2025-05-21 11:20:20 +0800

    setup connection to mongoDB

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 
 
 import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
 
 dotenv.config()
 const app = express()
@@ -21,4 +22,5 @@ app.use('/api/auth', authRoutes)
 
 app.listen( PORT, () => {
   console.log(`Greeting！Tarnished. Welcome to the Lands Between on trail ${PORT}！`)
+  connectDB()
 })
