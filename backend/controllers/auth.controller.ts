135a0c45942b5c08d90f951e40d9cf1d01d415ed
Chewsday <z647623@gmail.com>
2025-05-21 11:03:27 +0800

    setup base and auth routes

---
+++ backend/controllers/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+
+
+
+
+
+export async function signup (req: Request, res: Response) {
+  
+}
+
+
+export async function login (req: Request, res: Response) {
+  
+}
+
+
+export async function logout (req: Request, res: Response) {
+  
+}
---
+++ backend/server.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config()
+const app = express()
+
+
+const PORT = process.env.PORT || 9000
+
+
+
+
+app.use('/api/auth', authRoutes)
+
+
+
+
+
+app.listen( PORT, () => {
+  console.log(`Greeting！Tarnished. Welcome to the Lands Between on trail ${PORT}！`)
+})
