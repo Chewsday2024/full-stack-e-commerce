828025b22245be5a1b51bff25e4d01a3579de693
Chewsday <z647623@gmail.com>
2025-05-22 15:56:42 +0800

    payment routes done

---
+++ backend/types/stripeMeadata.d.ts
@@ -0,0 +1,13 @@
+import 'stripe'
+
+declare module 'stripe' {
+  interface Metadata {
+    userId: string
+    couponCode: string
+    products: {
+      id: string
+      quantity: number
+      price: number
+    }
+  }
+}
---
+++ backend/utils/createNewCoupon.ts
@@ -0,0 +1,14 @@
+import { Coupon } from "../models/coupon.model.js";
+
+export async function createNewCoupon (userId: string) {
+  const newCoupon = new Coupon({
+    code: 'GIFT' + Math.random().toString(36).substring(2, 8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    userId: userId
+  })
+
+  await newCoupon.save()
+
+  return newCoupon
+}
