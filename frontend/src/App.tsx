f5d694c77c75ae2ea37882c13600af221ac0082f
Chewsday <z647623@gmail.com>
2025-05-23 15:03:29 +0800

    setup routes and navbar done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,34 @@
+import { Route, Routes } from "react-router-dom"
+
+
+import HomePage from "./pages/HomePage"
+import SignupPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
+import Navbar from "./components/Navbar"
 
 function App() {
   
 
   return (
-    <>
-      <p className="text-9xl">dog</p>
-    </>
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-radial-[ellipse_at_top] from-[rgba(16,185,129,0.3)] from-0% via-[rgba(10,80,60,0.2)] via-45% to-[rgba(0,0,0,0.1)] to-100%" />
+        </div>
+      </div>
+
+
+
+      <div className="relative z-50 pt-20">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
